8927b917f8dc2bfe6970d8b66e059bcbf02dd452
MinhNguyen19 <tuanminh191919@gmail.com>
2021-10-05 22:07:18 -0400

    Initial commit

---
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.test1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        btn = findViewById(R.id.button);
+
+    }
+}
