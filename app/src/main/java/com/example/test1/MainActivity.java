f6b477f3ab5ae4d45c9459d8bb9137dfc6f506e1
MinhNguyen19 <tuanminh191919@gmail.com>
2021-10-05 22:48:26 -0400

    Test 2

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Test 2
 
         btn = findViewById(R.id.button);
 
