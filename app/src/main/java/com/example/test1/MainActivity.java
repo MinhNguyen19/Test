8600204993c89f7b239cd1583afd61e10df1d925
matt <sperky7933@gmail.com>
2021-10-05 23:10:17 -0400

    nw print

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -6,14 +6,17 @@ import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity { //test6
+
     Button btn;
 
     public void methodTest(){
         System.out.println("hi");
+        int a = 0;
 
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("hi");
         super.onCreate(savedInstanceState);
 
         btn = findViewById(R.id.button);
