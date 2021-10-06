5fa07e9460657d4266684ebd15b83ae83960d133
matt <sperky7933@gmail.com>
2021-10-05 22:30:15 -0400

    i modified x button to do y

--- app/src/main/java/com/example/test1/MainActivity.java
+++ app/src/main/java/com/example/test1/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity { //test1
+public class MainActivity extends AppCompatActivity { //test
     Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
